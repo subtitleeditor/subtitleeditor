9b4c0b3b0dfd1fca7d9059f586950523679fd6a2
felagund <tomashnyk@gmail.com>
2025-10-20 10:58:23 +0200

    Join Document Plugin: Add Append action and make offset configurable
    This adds new action that adds subtitles from another
    file without changing timecodes at all. This is useful if the subtitles
    are for the same video, for example if two people were working on the
    same video and now need to join their separate parts.
    
    Also, the offset of the original Join subtitle action is not
    configurable via a dialog. The dialog is also smart, as it offers
    the duration of video as the default offset if a video is open. When no
    video is open, the endtime of last subtitle is offered, mimicking
    current behaviour. Of course, the user can also choose something else.
    
    In practice, when joining subtitles like this, often they will be timed
    to a different video, that is supposedly going to be joined after the
    current video.
    
    Also a gramatical fix to translation files is applied.

--- plugins/actions/joindocument/joindocument.cc
+++ plugins/actions/joindocument/joindocument.cc
@@ -20,8 +20,50 @@
 
 #include <debug.h>
 #include <extension/action.h>
+#include <gtkmm_utility.h>
 #include <gui/dialogfilechooser.h>
+#include <gui/spinbuttontime.h>
 #include <i18n.h>
+#include <player.h>
+#include <subtitleeditorwindow.h>
+#include <utility.h>
+#include <widget_config_utility.h>
+
+#include <string>
+
+class DialogJoinOffset : public Gtk::Dialog {
+ public:
+  DialogJoinOffset(BaseObjectType *cobject,
+                   const Glib::RefPtr<Gtk::Builder> &builder)
+      : Gtk::Dialog(cobject) {
+    utility::set_transient_parent(*this);
+    builder->get_widget_derived("spin-offset", m_spinOffset);
+  }
+
+  void init(Document *doc, const Subtitle &last_subtitle) {
+    TIMING_MODE edit_mode = doc->get_edit_timing_mode();
+    m_spinOffset->set_timing_mode(edit_mode);
+
+    long default_offset = 0;
+
+    // Try to prefill with video end time
+    Player *player = SubtitleEditorWindow::get_instance()->get_player();
+    if (player && player->get_state() != Player::NONE)
+      default_offset = std::max<long>(0, player->get_duration());
+    else
+      default_offset = last_subtitle.get_end().totalmsecs;
+
+    m_spinOffset->set_value(static_cast<double>(default_offset));
+    m_spinOffset->grab_focus();
+  }
+
+  long get_offset_value() const {
+    return static_cast<long>(m_spinOffset->get_value());
+  }
+
+ protected:
+  SpinButtonTime *m_spinOffset;
+};
 
 class JoinDocumentPlugin : public Action {
  public:
@@ -41,9 +83,19 @@ class JoinDocumentPlugin : public Action {
     action_group = Gtk::ActionGroup::create("JoinDocumentPlugin");
 
     action_group->add(
-        Gtk::Action::create("join-document", Gtk::Stock::CONNECT,
-                            _("_Join Document"), _("Add subtitles from file")),
-        sigc::mem_fun(*this, &JoinDocumentPlugin::on_execute));
+        Gtk::Action::create(
+            "join-document", Gtk::Stock::CONNECT, _("_Join Document"),
+            _("Add subtitles from a file to the current document, adjusting "
+              "timecodes by given offset. If a video is open, its duration is "
+              "offered as the offset. If no video is open, the end time of the "
+              "last subtitle is offered as the offset.")),
+        sigc::mem_fun(*this, &JoinDocumentPlugin::on_execute_join));
+
+    action_group->add(
+        Gtk::Action::create(
+            "append-document", Gtk::Stock::ADD, _("_Append Document"),
+            _("Append subtitles from file without changing timecodes")),
+        sigc::mem_fun(*this, &JoinDocumentPlugin::on_execute_append));
 
     // ui
     Glib::RefPtr<Gtk::UIManager> ui = get_ui_manager();
@@ -54,6 +106,8 @@ class JoinDocumentPlugin : public Action {
 
     ui->add_ui(ui_id, "/menubar/menu-tools/join-document", "join-document",
                "join-document");
+    ui->add_ui(ui_id, "/menubar/menu-tools/append-document", "append-document",
+               "append-document");
   }
 
   void deactivate() {
@@ -71,81 +125,133 @@ class JoinDocumentPlugin : public Action {
     bool visible = (get_current_document() != NULL);
 
     action_group->get_action("join-document")->set_sensitive(visible);
+    action_group->get_action("append-document")->set_sensitive(visible);
   }
 
  protected:
-  void on_execute() {
+  bool applying_offset;
+  void on_execute_join() {
     se_dbg(SE_DBG_PLUGINS);
-
-    execute();
+    applying_offset = true;
+    execute(applying_offset);
   }
 
-  bool execute() {
+  void on_execute_append() {
+    se_dbg(SE_DBG_PLUGINS);
+    applying_offset = false;
+    execute(applying_offset);
+  }
+
+  bool execute(bool applying_offset) {
     se_dbg(SE_DBG_PLUGINS);
 
     Document *doc = get_current_document();
 
     g_return_val_if_fail(doc, false);
 
+    // Get the original document info before opening the file dialog
+    unsigned int subtitle_size = doc->subtitles().size();
+
     DialogOpenDocument::unique_ptr ui = DialogOpenDocument::create();
 
     ui->show_video(false);
     ui->set_select_multiple(false);
 
-    if (ui->run() == Gtk::RESPONSE_OK) {
-      Glib::ustring uri = ui->get_uri();
-      // tmp document to try to open the file
-      Document *tmp = Document::create_from_file(uri);
-      if (tmp == NULL)
+    if (ui->run() != Gtk::RESPONSE_OK)
+      return false;
+
+    Glib::ustring uri = ui->get_uri();
+
+    // tmp document to try to open the file
+    Document *tmp = Document::create_from_file(uri);
+    if (tmp == NULL)
+      return false;
+
+    ui->hide();  // hides (closes?) the dialog so the next one can display
+
+    Glib::ustring encoding = tmp->getCharset();
+    delete tmp;
+
+    // Declare offset variable outside the if block so it's available later
+    SubtitleTime offset = 0;
+
+    // If we are joining, we ask for offset first, otherwise the subtitles
+    // get joined without offset in the background, which looks ugly
+    if (applying_offset) {
+      // Get the last subtitle of the original document
+      Subtitle last_orig_sub = doc->subtitles().get(subtitle_size);
+
+      // Show offset dialog
+      std::unique_ptr<DialogJoinOffset> offset_dialog(
+          gtkmm_utility::get_widget_derived<DialogJoinOffset>(
+              SE_DEV_VALUE(SE_PLUGIN_PATH_UI, SE_PLUGIN_PATH_DEV),
+              "dialog-join-offset.ui", "dialog-join-offset"));
+
+      offset_dialog->init(doc, last_orig_sub);
+
+      if (offset_dialog->run() == Gtk::RESPONSE_OK) {
+        offset = offset_dialog->get_offset_value();
+      } else {
+        doc->flash_message(_("Join cancelled."));
         return false;
-
-      Glib::ustring ofile = doc->getFilename();
-      Glib::ustring oformat = doc->getFormat();
-      Glib::ustring ocharset = doc->getCharset();
-
-      Glib::ustring encoding = tmp->getCharset();
-
-      delete tmp;
-
-      unsigned int subtitle_size = doc->subtitles().size();
-
-      try {  // needs with Document::open
-        doc->start_command(_("Join document"));
-        doc->setCharset(encoding);
-        doc->open(uri);
-
-        // Moves added subtitles after the last original
-        if (subtitle_size > 0) {
-          // Get the last subtitle of the original document
-          Subtitle last_orig_sub = doc->subtitles().get(subtitle_size);
-          // Get The first subtitle added to the original document
-          Subtitle first_new_subs = doc->subtitles().get_next(last_orig_sub);
-
-          // The offset from the last original sub
-          SubtitleTime offset = last_orig_sub.get_end();
-          for (Subtitle sub = first_new_subs; sub; ++sub) {
-            sub.set_start_and_end(sub.get_start() + offset,
-                                  sub.get_end() + offset);
-          }
-          // Make the user life easy by selecting the first new subtitle
-          doc->subtitles().select(first_new_subs);
-        }
-
-        doc->setFilename(ofile);
-        doc->setFormat(oformat);
-        doc->setCharset(ocharset);
-        doc->finish_command();
-
-        unsigned int subtitles_added = doc->subtitles().size() - subtitle_size;
-
-        doc->flash_message(
-            ngettext("1 subtitle has been added at this document.",
-                     "%d subtitles have been added at this document.",
-                     subtitles_added),
-            subtitles_added);
-      } catch (...) {
-        se_dbg_msg(SE_DBG_PLUGINS, "Failed to join document: %s", uri.c_str());
       }
+    }
+
+    Glib::ustring ofile = doc->getFilename();
+    Glib::ustring oformat = doc->getFormat();
+    Glib::ustring ocharset = doc->getCharset();
+
+    try {  // needs with Document::open
+      doc->start_command(applying_offset ? _("Join document")
+                                         : _("Append document"));
+      doc->setCharset(encoding);
+      doc->open(uri);
+
+      // Get The first subtitle added to the original document
+      Subtitle last_orig_sub = doc->subtitles().get(subtitle_size);
+      Subtitle first_new_subs = doc->subtitles().get_next(last_orig_sub);
+
+      // Now we apply offset
+      if (applying_offset) {
+        SubtitleTime min_gap =
+            cfg::get_int("timing", "min-gap-between-subtitles");
+        SubtitleTime gap = offset - last_orig_sub.get_end();
+        se_dbg_msg(SE_DBG_PLUGINS, "First new %s",
+                   first_new_subs.get_start().str().c_str());
+        se_dbg_msg(SE_DBG_PLUGINS, "Offset %s", offset.str().c_str());
+        se_dbg_msg(SE_DBG_PLUGINS, "Last_orig %s",
+                   last_orig_sub.get_end().str().c_str());
+        se_dbg_msg(SE_DBG_PLUGINS, "Min_gap %s", min_gap.str().c_str());
+        se_dbg_msg(SE_DBG_PLUGINS, "Gap %s", gap.str().c_str());
+        if (gap < min_gap)
+          offset = offset + min_gap - gap;
+
+        for (Subtitle sub = first_new_subs; sub; ++sub) {
+          sub.set_start_and_end(sub.get_start() + offset,
+                                sub.get_end() + offset);
+        }
+      }
+
+      // Make the user life easy by selecting the first new subtitle
+      if (first_new_subs) {
+        doc->subtitles().select(first_new_subs);
+      }
+
+      doc->setFilename(ofile);
+      doc->setFormat(oformat);
+      doc->setCharset(ocharset);
+      doc->finish_command();
+
+      unsigned int subtitles_added = doc->subtitles().size() - subtitle_size;
+
+      doc->flash_message(
+          ngettext("One subtitle has been added to this document.",
+                   "%d subtitles have been added to this document.",
+                   subtitles_added),
+          subtitles_added);
+    } catch (...) {
+      se_dbg_msg(SE_DBG_PLUGINS, "Failed to %s document: %s",
+                 applying_offset ? "join" : "append", uri.c_str());
     }
 
     return true;
