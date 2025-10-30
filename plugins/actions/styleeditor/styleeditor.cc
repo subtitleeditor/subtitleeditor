83204dfdc015f79a59a25d98ac9ef543c8443188
felagund <tomashnyk@gmail.com>
2025-10-30 14:11:15 +0100

    Styleeditor and ASS: overall improvements
    - Added tooltips to Style editor.
    - Added new button to set default style, so the user does not have to manually edit the config file.
    - Added option (works only with libass) to display an even rectangular box as subtitle background.
    - Revamped PlayResX and PlayResY support. Now the screen size is not used implicitly, but we explicitly use a default value of 1920x1080. That also applies when a new file is created. Users can change this via a dialog. When encountering a file with no PlayRes set, we do not set it unless a new style is added, in which case we ask the user for confirmation.
    - Changing styles now emits a signal that the document has changed, so the label in its tab gets an asterisk to indicate that.
    - Moved some ASS function to utility, so they can be reused both by the Style editor and ASS file plugin.
    - Also when adding or duplicating a style, its name is focused and open
      for renaming, so the user can do that right away. (fixes https://web.archive.org/web/20170205081633/http://gna.org/bugs/?23574 )

--- plugins/actions/styleeditor/styleeditor.cc
+++ plugins/actions/styleeditor/styleeditor.cc
@@ -27,6 +27,7 @@
 #include <utility.h>
 
 #include <memory>
+//#include "gtkmm/messagedialog.h"
 
 class ColumnNameRecorder : public Gtk::TreeModel::ColumnRecord {
  public:
@@ -35,6 +36,11 @@ class ColumnNameRecorder : public Gtk::TreeModel::ColumnRecord {
   }
   Gtk::TreeModelColumn<Glib::ustring> name;
 };
+
+// During dialog initialization, we need to block signals that the document has changed,
+// which triggered changing document's label by adding an asterisk to the filename to indicate that the document
+// has changed
+bool m_block_signals = false;
 
 DialogStyleEditor::DialogStyleEditor(BaseObjectType *cobject,
                                      const Glib::RefPtr<Gtk::Builder> &builder)
@@ -49,7 +55,6 @@ DialogStyleEditor::DialogStyleEditor(BaseObjectType *cobject,
         sigc::bind(sigc::mem_fun(*this, &DialogStyleEditor::callback), w, \
                    Glib::ustring(key)));                                  \
   }
-
   builder->get_widget("vbox-style", m_widgets["vbox-style"]);
 
   init_widget(Gtk::Button, "button-new-style", signal_clicked,
@@ -58,6 +63,13 @@ DialogStyleEditor::DialogStyleEditor(BaseObjectType *cobject,
               callback_button_clicked, "delete-style");
   init_widget(Gtk::Button, "button-copy-style", signal_clicked,
               callback_button_clicked, "copy-style");
+  init_widget(Gtk::Button, "button-set-default", signal_clicked,
+              callback_button_clicked, "set-default");
+
+  init_widget(Gtk::SpinButton, "spin-playres-x", signal_value_changed,
+            callback_playres_changed, "playres-x");
+  init_widget(Gtk::SpinButton, "spin-playres-y", signal_value_changed,
+            callback_playres_changed, "playres-y");
 
   init_widget(Gtk::FontButton, "button-font", signal_font_set,
               callback_font_button_changed, "font");
@@ -99,10 +111,13 @@ DialogStyleEditor::DialogStyleEditor(BaseObjectType *cobject,
               callback_spin_value_changed, "outline");
   init_widget(Gtk::SpinButton, "spin-shadow", signal_value_changed,
               callback_spin_value_changed, "shadow");
+
   init_widget(Gtk::RadioButton, "radio-outline", signal_toggled,
               callback_radio_toggled, "outline");
-  init_widget(Gtk::RadioButton, "radio-opaque-box", signal_toggled,
-              callback_radio_toggled, "opaque-box");
+  init_widget(Gtk::RadioButton, "radio-box-per-line", signal_toggled,
+              callback_radio_toggled, "box-per-line");
+  init_widget(Gtk::RadioButton, "radio-rectangular-box", signal_toggled,
+              callback_radio_toggled, "rectangular-box");
 
   for (unsigned int i = 0; i < 9; ++i) {
     Glib::ustring b = build_message("button-alignment-%d", i + 1);
@@ -114,7 +129,7 @@ DialogStyleEditor::DialogStyleEditor(BaseObjectType *cobject,
         i + 1));
   }
 
-  // create treeview
+  // create treeview (this is in braces to destroy the pointers after we do not need them)
   {
     Gtk::TreeViewColumn *column = NULL;
     Gtk::CellRendererText *renderer = NULL;
@@ -140,21 +155,11 @@ DialogStyleEditor::DialogStyleEditor(BaseObjectType *cobject,
 
     m_treeview->get_selection()->signal_changed().connect(sigc::mem_fun(
         *this, &DialogStyleEditor::callback_style_selection_changed));
-    // add styles
-    // m_current_document = se::documents::active();
-    //
-    // for (Style style = m_current_document->styles().first(); style; ++style)
-    // {
-    //   Gtk::TreeIter iter = m_liststore->append();
-    //
-    //   (*iter)[column_name.name] = style.get("name");
-    // }
-    //
-    // if (m_liststore->children().empty()) {
-    //   m_widgets["vbox-style"]->set_sensitive(false);
-    // } else {
-    //   m_treeview->get_selection()->select(m_liststore->children().begin());
-    // }
+    if (m_liststore->children().empty()) {
+      m_widgets["vbox-style"]->set_sensitive(false);
+    } else {
+    m_treeview->get_selection()->select(m_liststore->children().begin());
+    }
   }
 }
 
@@ -170,45 +175,118 @@ void DialogStyleEditor::on_style_name_edited(const Glib::ustring &path,
     (*iter)[column_name.name] = text;
 
     style.set("name", text);
+    m_current_document->make_document_changed();
   }
 }
 
 void DialogStyleEditor::callback_button_clicked(Gtk::Button *,
                                                 const Glib::ustring &action) {
+  ScriptInfo &script_info = m_current_document->get_script_info();
   if (action == "new-style") {
+    // If There are already styles and PlayRes is not set, ask user what to do as just adding it might corrupt their styles
+    if (m_current_document->styles().size() > 0 &&  (!script_info.data.count("PlayResX") || !script_info.data.count("PlayResX"))) {
+       m_current_document->flash_message(_("Problems"));
+       const int RESPONSE_APPLY_DEFAULT = 1;
+       Gtk::MessageDialog dialog(*this,
+                                _("PlayResX or PlayResY not set"),
+                                false,
+                                Gtk::MESSAGE_INFO,
+                                Gtk::BUTTONS_CANCEL);
+       dialog.set_title(_("Missing PlayRes Information"));
+       dialog.set_secondary_text("You tried to add a new style, but this document does not have target screen size set and already has styles.\n\n Either cancel (no style will be added then) and fix the situation manually.\n\nOr apply a default screen size (1920x1080). A new style will be added then, but it might make subtitles in the current styles look too big or too small");
+       dialog.add_button(_("Apply default PlayRes"), RESPONSE_APPLY_DEFAULT);
+
+       int result = dialog.run();
+       if (result != RESPONSE_APPLY_DEFAULT)
+           return;
+       else {
+           ASS::set_default_playres(script_info);
+           dynamic_cast<Gtk::SpinButton *>(m_widgets["spin-playres-x"])
+        ->set_sensitive(true);
+           dynamic_cast<Gtk::SpinButton *>(m_widgets["spin-playres-y"])
+        ->set_sensitive(true);
+       }
+    }
+    // If There are no styles and PlayRes is not set, just add Playres
+    if (m_current_document->styles().size() == 0 &&  (!script_info.data.count("PlayResX") || !script_info.data.count("PlayResX"))) {
+        ASS::set_default_playres(script_info);
+           dynamic_cast<Gtk::SpinButton *>(m_widgets["spin-playres-x"])
+        ->set_sensitive(true);
+           dynamic_cast<Gtk::SpinButton *>(m_widgets["spin-playres-y"])
+        ->set_sensitive(true);
+    }
     ColumnNameRecorder column;
     Gtk::TreeIter iter = m_liststore->append();
 
     (*iter)[column.name] = "Undefined";
 
     Style style = m_current_document->styles().append();
+    ASS::set_default_style(style);
     style.set("name", "Undefined");
+
+    // Select the new style and start editing
+    m_treeview->get_selection()->select(iter);
+    Gtk::TreePath path = m_liststore->get_path(iter);
+    Gtk::TreeViewColumn* col = m_treeview->get_column(0);
+    m_treeview->set_cursor(path, *col, true);
+    m_current_document->make_document_changed();
+
   } else if (action == "delete-style") {
     if (m_current_style) {
       m_current_document->styles().remove(m_current_style);
 
       Gtk::TreeIter iter = m_treeview->get_selection()->get_selected();
       m_liststore->erase(iter);
+      m_current_document->make_document_changed();
     }
   } else if (action == "copy-style") {
     if (m_current_style) {
       Style new_style = m_current_document->styles().append();
 
       m_current_style.copy_to(new_style);
-      new_style.set("name", new_style.get("name") + "#");
+      new_style.set("name", new_style.get("name") + " (Copy)");
       //
       ColumnNameRecorder column;
       Gtk::TreeIter iter = m_liststore->append();
       (*iter)[column.name] = new_style.get("name");
 
       m_treeview->get_selection()->select(iter);
+
+      // Start editing the name immediately
+      Gtk::TreePath path = m_liststore->get_path(iter);
+      Gtk::TreeViewColumn* col = m_treeview->get_column(0);
+      m_treeview->set_cursor(path, *col, true);
+      m_current_document->make_document_changed();
+    }
+  } else if (action == "set-default") {
+    if (m_current_style) {
+      Glib::ustring default_style = ASS::style_to_string(m_current_style);
+      cfg::set_string("AdvancedSubStationAlpha", "default-style", default_style);
+      cfg::set_comment("AdvancedSubStationAlpha", "default-style",
+                         "Default style to be used");
+      // We save PlayRes only if it is set (it can only be unset when a file without it is opened)
+      bool playres_saved_to_config = false;
+      if (script_info.data.count("PlayResX")) {
+        Glib::ustring playres_x = script_info.data["PlayResX"];
+        cfg::set_string("AdvancedSubStationAlpha", "default-playres-x",playres_x);
+        playres_saved_to_config = true;
+      }
+      if (script_info.data.count("PlayResY")) {
+        Glib::ustring playres_y = script_info.data["PlayResY"];
+        cfg::set_string("AdvancedSubStationAlpha", "default-playres-y",playres_y);
+        playres_saved_to_config = true;
+      }
+      if (playres_saved_to_config)
+        m_current_document->flash_message(_("Default Style and PlayRes saved to configuration file"));
+      else
+        m_current_document->flash_message(_("Default Style saved to configuration file"));
     }
   }
 }
 
 void DialogStyleEditor::callback_font_button_changed(Gtk::FontButton *w,
                                                      const Glib::ustring &) {
-  if (!m_current_style)
+  if (!m_current_style || m_block_signals)
     return;
 
   Pango::FontDescription description(w->get_font_name());
@@ -218,34 +296,40 @@ void DialogStyleEditor::callback_font_button_changed(Gtk::FontButton *w,
 
   m_current_style.set("font-name", font_name);
   m_current_style.set("font-size", font_size);
+  m_current_document->make_document_changed();
 }
 
 void DialogStyleEditor::callback_button_toggled(Gtk::ToggleButton *w,
                                                 const Glib::ustring &key) {
-  if (!m_current_style)
+  if (!m_current_style || m_block_signals)
     return;
 
   m_current_style.set(key, to_string(w->get_active()));
+  m_current_document->make_document_changed();
 }
 
 void DialogStyleEditor::callback_spin_value_changed(Gtk::SpinButton *w,
                                                     const Glib::ustring &key) {
-  if (!m_current_style)
+  if (!m_current_style || m_block_signals)
     return;
 
   m_current_style.set(key, to_string(w->get_value()));
+  m_current_document->make_document_changed();
 }
 
 void DialogStyleEditor::callback_radio_toggled(Gtk::RadioButton *w,
                                                const Glib::ustring &key) {
-  if (!m_current_style)
+  if (!m_current_style || m_block_signals)
     return;
 
   if (w->get_active()) {
     if (key == "outline")
       m_current_style.set("border-style", "1");
-    else if (key == "opaque-box")
+    else if (key == "box-per-line")
       m_current_style.set("border-style", "3");
+    else if (key == "rectangular-box")
+      m_current_style.set("border-style", "4");
+    m_current_document->make_document_changed();
   }
 }
 void DialogStyleEditor::callback_color_button(Gtk::ColorButton *w,
@@ -257,6 +341,7 @@ void DialogStyleEditor::callback_color_button(Gtk::ColorButton *w,
   color.getFromColorButton(*w);
 
   m_current_style.set(key, color.to_string());
+  m_current_document->make_document_changed();
 }
 
 void DialogStyleEditor::callback_style_selection_changed() {
@@ -276,17 +361,59 @@ void DialogStyleEditor::callback_style_selection_changed() {
 
 void DialogStyleEditor::callback_alignment_changed(Gtk::RadioButton *w,
                                                    unsigned int num) {
-  if (!m_current_style)
+  if (!m_current_style || m_block_signals)
     return;
 
   if (w->get_active())
     m_current_style.set("alignment", to_string(num));
+  m_current_document->make_document_changed();
 }
 
+void DialogStyleEditor::callback_playres_changed(Gtk::SpinButton *w,
+const Glib::ustring &key) {
+  if (!m_current_document || m_block_signals)
+    return;
+
+  ScriptInfo &script_info = m_current_document->get_script_info();
+
+  if (key == "playres-x") {
+    script_info.data["PlayResX"] = to_string(static_cast<int>(w->get_value()));
+  } else if (key == "playres-y") {
+    script_info.data["PlayResY"] = to_string(static_cast<int>(w->get_value()));
+  }
+  m_current_document->make_document_changed();
+}
+
+
 void DialogStyleEditor::init_style(const Style &style) {
-  std::cout << "init_style: " << ((style) ? style.get("name") : "null")
-            << std::endl;
+  se_dbg_msg(SE_DBG_PLUGINS, ("init_style: " + (style ? style.get("name") : "null")).c_str());
+  m_block_signals = true;
   m_current_style = style;
+  // Load PlayRes always (not just when style is selected)
+  if (m_current_document) {
+    ScriptInfo &script_info = m_current_document->get_script_info();
+
+    int playres_x = 0;
+    int playres_y = 0;
+
+    if (script_info.data.count("PlayResX")) {
+        playres_x = utility::string_to_int(script_info.data["PlayResX"]);
+        dynamic_cast<Gtk::SpinButton *>(m_widgets["spin-playres-x"])
+          ->set_value(playres_x);
+    } else {
+        dynamic_cast<Gtk::SpinButton *>(m_widgets["spin-playres-x"])
+        ->set_sensitive(false);
+    }
+
+    if (script_info.data.count("PlayResY")) {
+        playres_y = utility::string_to_int(script_info.data["PlayResY"]);
+        dynamic_cast<Gtk::SpinButton *>(m_widgets["spin-playres-y"])
+          ->set_value(playres_y);
+    } else {
+        dynamic_cast<Gtk::SpinButton *>(m_widgets["spin-playres-y"])
+          ->set_sensitive(false);
+    }
+  }
 
   m_widgets["vbox-style"]->set_sensitive((m_current_style));
 
@@ -342,8 +469,11 @@ void DialogStyleEditor::init_style(const Style &style) {
     if (border_style == "1")
       dynamic_cast<Gtk::RadioButton *>(m_widgets["radio-outline"])
           ->set_active(true);
+    else if (border_style == "4")
+      dynamic_cast<Gtk::RadioButton *>(m_widgets["radio-rectangular-box"])
+          ->set_active(true);
     else
-      dynamic_cast<Gtk::RadioButton *>(m_widgets["radio-opaque-box"])
+      dynamic_cast<Gtk::RadioButton *>(m_widgets["radio-box-per-line"])
           ->set_active(true);
   }
   // alignment
@@ -353,6 +483,7 @@ void DialogStyleEditor::init_style(const Style &style) {
     dynamic_cast<Gtk::RadioButton *>(m_widgets["button-alignment-" + num])
         ->set_active(true);
   }
+  m_block_signals = false;
 }
 
 void DialogStyleEditor::execute(Document *doc) {
@@ -434,24 +565,24 @@ class StyleEditorPlugin : public Action {
   }
 
  protected:
-  void on_execute() {
-    se_dbg(SE_DBG_PLUGINS);
+   void on_execute() {
+     se_dbg(SE_DBG_PLUGINS);
 
-    Document *doc = get_current_document();
-    g_return_if_fail(doc);
+     Document *doc = get_current_document();
+     g_return_if_fail(doc);
 
-    // create dialog
-    std::unique_ptr<DialogStyleEditor> dialog(
-        gtkmm_utility::get_widget_derived<DialogStyleEditor>(
-            SE_DEV_VALUE(SE_PLUGIN_PATH_UI, SE_PLUGIN_PATH_DEV),
-            "dialog-style-editor.ui", "dialog-style-editor"));
+     // create dialog
+     std::unique_ptr<DialogStyleEditor> dialog(
+         gtkmm_utility::get_widget_derived<DialogStyleEditor>(
+             SE_DEV_VALUE(SE_PLUGIN_PATH_UI, SE_PLUGIN_PATH_DEV),
+             "dialog-style-editor.ui", "dialog-style-editor"));
 
-    dialog->execute(doc);
-  }
+     dialog->execute(doc);
+   }
 
  protected:
-  Gtk::UIManager::ui_merge_id ui_id;
-  Glib::RefPtr<Gtk::ActionGroup> action_group;
+   Gtk::UIManager::ui_merge_id ui_id;
+   Glib::RefPtr<Gtk::ActionGroup> action_group;
 };
 
 REGISTER_EXTENSION(StyleEditorPlugin)
